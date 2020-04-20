fbfc5b3eec8e3dbfda0fa754fca2cf77c252433d
komal <khupsekomal@gmail.com>
2020-04-20 17:28:54 +0530

    Using dependency of devtools for reloading application.

--- src/main/java/com/practice/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/practice/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -3,15 +3,29 @@ package com.practice.springboot.demo.mycoolapp.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @RestController
 public class FunRestController {
-    // Expose "/" that return HelloWorld.
 
+
+    // Expose "/" that return HelloWorld.
     @GetMapping("/")
-    public String sayHello(){
-        return "Hello world ! Time on Server is "+ LocalDateTime.now();
+    public String sayHello() {
+        return "Hello world ! Time on Server is " + LocalDateTime.now();
     }
+
+
+    //Expose "/workout" that return output of workout
+    @GetMapping("/workout")
+    public String getWorkout(){
+        return "Run hard upto 5 k.";
+    }
+
+    //Expose "/fortune" that return daily fortune
+    @GetMapping("/fortune")
+    public String getFortune(){
+        return "This is your lucky day.";
+    }
+
 }
